3c20d9a1f59a8047a7e27c70d6ccc611f86b360d
maxusikk <165891004+maxusikk@users.noreply.github.com>
2024-05-04 11:47:20 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,2 +1,16 @@
+// Main.java
+
+import company.beans.Person;
+import company.exception.InvalidAgeException;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            Person person = new Person("Damian", 30);
+            System.out.println("Name: " + person.getName());
+            System.out.println("Age: " + person.getAge());
+        } catch (InvalidAgeException e) {
+            System.out.println("Invalid age: " + e.getMessage());
+        }
+    }
 }
