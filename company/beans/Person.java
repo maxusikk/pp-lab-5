11339f98fcbbc16d02caadebd04c42381d66011f
Damian Ziółkowski <xdamianeeg@gmail.com>
2024-05-04 11:47:57 +0200

    :)

--- company/beans/Person.java
+++ company/beans/Person.java
@@ -1,2 +1,37 @@
-package company.beans;public class Person {
+// company/beans/Person.java
+
+package company.beans;
+
+import company.exception.InvalidAgeException;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) throws InvalidAgeException {
+        this.name = name;
+        if (age < 0 || age > 125) {
+            throw new InvalidAgeException("Invalid age: " + age);
+        }
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) throws InvalidAgeException {
+        if (age < 0 || age > 125) {
+            throw new InvalidAgeException("Invalid age: " + age);
+        }
+        this.age = age;
+    }
 }
--- company/exceptions/InvalidAgeException.java
+++ company/exceptions/InvalidAgeException.java
@@ -1,2 +1,9 @@
-package company.exceptions;public class InvalidAgeException {
+// company/exception/InvalidAgeException.java
+
+package company.exception;
+
+public class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
 }
