afec39624756102b07b458d171e8a34303147f18
Damian Ziółkowski <xdamianeeg@gmail.com>
2024-05-04 11:53:23 +0200

    :)

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
-// Main.java
-
 import company.beans.Person;
-import company.exception.InvalidAgeException;
+import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
 
 public class Main {
-    public static void main(String[] args) {
+    public static <InvalidAgeException> void main(String[] args) {
         try {
-            Person person = new Person("John", 30);
+            Person person = new Person("Mia Khalifa", 31);
             System.out.println("Name: " + person.getName());
             System.out.println("Age: " + person.getAge());
+
+            Messenger emailMessenger = new EmailMessenger();
+            emailMessenger.sendMessage("Hello Mia, thank you for the most enjoyable experience yesterday");
         } catch (InvalidAgeException e) {
-            System.out.println("Invalid age: " + e.getMessage());
+            System.err.println(e.getMessage());
         }
     }
 }
