cb0fb7c8c8ea119f8bfabf3a04dcf637b4b12040
Damian Ziółkowski <xdamianeeg@gmail.com>
2024-05-04 11:55:58 +0200

    :)

--- Main.java
+++ Main.java
@@ -2,16 +2,20 @@ import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.interfaces.Messenger;
+import company.utils.MathUtils;
 
 public class Main {
-    public static <InvalidAgeException> void main(String[] args) {
+    public static void main(String[] args) {
         try {
             Person person = new Person("Mia Khalifa", 31);
             System.out.println("Name: " + person.getName());
             System.out.println("Age: " + person.getAge());
 
+            int result = MathUtils.add(164, 12);
+            System.out.println("Result: " + result);
+
             Messenger emailMessenger = new EmailMessenger();
-            emailMessenger.sendMessage("Hello Mia, thank you for the most enjoyable experience yesterday");
+            emailMessenger.sendMessage("Hello, Mia! The math result you asked for is " + result + ".");
         } catch (InvalidAgeException e) {
             System.err.println(e.getMessage());
         }
